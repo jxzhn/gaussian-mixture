54ef12397d0d7c2fbc2eebdfca9360e02f845f31
jxzhn <jxzhn@jxzhn.com>
2021-06-21 13:36:54 +0800

    修改单元测试测例

--- unit_test/matCholesky.py
+++ unit_test/matCholesky.py
@@ -1,5 +1,6 @@
 import ctypes
 import numpy as np
+import scipy.linalg
 
 gmm_matrix_support = ctypes.cdll.LoadLibrary('./libgmm_matrix_support.so')
 # 设置参数类型
@@ -9,11 +10,11 @@ gmm_matrix_support.matCholesky.argtypes = [
     ctypes.c_int
 ]
 
-testcase = np.random.randn(1000, 1000).astype(np.float32) * 2.33 + 0.66
-testcase = np.dot(testcase, testcase.T) # 保证对称且正定
-answer = np.linalg.cholesky(testcase)
+X = np.random.randn(1000, 784).astype(np.float32) * 2.33 + 0.66
+testcase = np.cov(X, rowvar=False).astype(np.float32)
+answer = scipy.linalg.cholesky(testcase, lower=True)
 
-output = np.empty((1000, 1000), dtype=np.float32)
+output = np.empty((784, 784), dtype=np.float32)
 
 gmm_matrix_support.matCholesky(testcase, output, testcase.shape[0])
 
--- unit_test/solveLower.py
+++ unit_test/solveLower.py
@@ -1,5 +1,6 @@
 import ctypes
 import numpy as np
+import scipy.linalg
 
 gmm_matrix_support = ctypes.cdll.LoadLibrary('./libgmm_matrix_support.so')
 # 设置参数类型
@@ -11,21 +12,18 @@ gmm_matrix_support.solveLower.argtypes = [
     ctypes.c_int
 ]
 
-testcase = np.random.randn(1000,1000).astype(np.float32) * 2.33 + 0.66
-testcase
-testcase = np.tril(testcase, k=0)
+X = np.random.randn(1000, 784).astype(np.float32) * 2.33 + 0.66
+covarianceX = np.cov(X, rowvar=False).astype(np.float32)
 
-bmat = np.random.randn(500,1000).astype(np.float32) * 2.33 + 0.66
+testcase = np.ascontiguousarray(scipy.linalg.cholesky(covarianceX, lower=True), dtype=np.float32)
 
-output = np.empty((500,1000), dtype=np.float32)
+answer = scipy.linalg.solve_triangular(testcase, X.T, lower=True).T
 
-answer = np.linalg.solve(testcase,bmat.T)
-# print((answer == answer).all())
-
-gmm_matrix_support.solveLower(testcase, bmat, output, testcase.shape[0],bmat.shape[0])
+output = np.empty((1000, 784), dtype=np.float32)
+gmm_matrix_support.solveLower(testcase, X, output, X.shape[1], X.shape[0])
 
 
-diff = np.abs(output - answer.T).max()
+diff = np.abs(output - answer).max()
 if diff < 1e-3:
     print('test passed.')
 else:
